9a7f2f15c9e6539a466e176617c52a37df29f2f7
Akhil Sajeevan <akhilsajeevan@cygnonex.com>
2025-01-24 16:10:30 +0530

    upload folder removed

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-import os
-
 class Config:
-    UPLOAD_FOLDER = 'uploads'
     # Other configuration variables
 
     @staticmethod
     def init_app(app):
-        # Create upload directory if it doesn't exist
-        if not os.path.exists(app.config['UPLOAD_FOLDER']):
-            os.makedirs(app.config['UPLOAD_FOLDER'])
+       
+        pass
